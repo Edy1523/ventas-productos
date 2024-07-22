26e99f49207c312ac0f0cb1837cf0fb6a5309578
Edy1523 <edwar.18mh@gmail.com>
2024-07-21 20:01:10 -0500

    Revert "coneccion a la base de datos serverless, creacion de las tablas con el ORM"
    This reverts commit b1bb7b1699a50624b987f5317b13a3a9535d0ab6.

--- models/clients.py
+++ models/clients.py
@@ -1,11 +1,8 @@
-from service.database_connection import Base
-from sqlalchemy import Column, Integer, String, BIGINT
+from sqlalchemy import Column, Integer, String
 
-class Clients(Base):
-    __tablename__ = "clientes"
+class Clients:
+    __tablename__ = "clients"
     
     id = Column(Integer, primary_key=True,autoincrement=True)
-    nombre_cliente = Column(String(30), nullable=False)
-    correo = Column(String(25), nullable=False)
-    telefono = Column(BIGINT)
-    direccion = Column(String(30), nullable=False)
+    nombre = Column(String)
+    telefono = Column(Integer)
--- models/login_employees.py
+++
@@ -1,9 +0,0 @@
-from service.database_connection import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-
-class LoginEmployees(Base):
-    __tablename__ = "login_empleados"
-    
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    id_empleado = Column(Integer, ForeignKey("empleados.id"), nullable=False)
-    contrasena = Column(String(100), nullable=False)
