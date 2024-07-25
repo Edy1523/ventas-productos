bd6d9af5591e5b3a897a1fb17b56ab61e8858bfd
Edy1523 <edwar.18mh@gmail.com>
2024-07-25 09:34:33 -0500

    Cambios de estructura en el design document, eliminación de archivos sobrantes

--- models/clients.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-
-class Clients:
-    __tablename__ = "clients"
-    
-    id = Column(Integer, primary_key=True,autoincrement=True)
-    nombre = Column(String)
-    telefono = Column(Integer)
